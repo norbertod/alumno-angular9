101dca9cee9a22a4bea90985e836dca9fababc13
norbertod <norbertod.rojas@gmail.com>
2020-09-21 18:04:13 -0300

    clase 73

--- microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/models/entity/Curso.java
+++ microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/models/entity/Curso.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -16,43 +17,49 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotEmpty;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
 import com.formacionbdi.microservicios.commons.examenes.models.entity.Examen;
 
 @Entity
-@Table(name = "cursos")
+@Table(name="cursos")
 public class Curso {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	@NotEmpty
 	private String nombre;
-	@Column(name = "create_at")
+	
+	@Column(name="create_at")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date createAt;
 	
-	@OneToMany(fetch = FetchType.LAZY) // el el dueño de la relacion, al ser unidireccional, crea una tabla
-	private List<Alumno> alumnos;		// con los id de Curso y Alumno
+	@JsonIgnoreProperties(value= {"curso"}, allowSetters = true)
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "curso", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<CursoAlumno> cursoAlumnos;
+	
+	// @OneToMany(fetch = FetchType.LAZY)
+	@Transient
+	private List<Alumno> alumnos;
 	
 	@ManyToMany(fetch = FetchType.LAZY)
 	private List<Examen> examenes;
-	
+
 	@PrePersist
 	public void prePersist() {
 		this.createAt = new Date();
 	}
-	
-	
 
 	public Curso() {
 		this.alumnos = new ArrayList<>();
 		this.examenes = new ArrayList<>();
+		this.cursoAlumnos = new ArrayList<>();
 	}
-
-
 
 	public Long getId() {
 		return id;
@@ -89,23 +96,19 @@ public class Curso {
 	public void addAlumno(Alumno alumno) {
 		this.alumnos.add(alumno);
 	}
-	
-	public void removeAlumnos(Alumno alumno) {
+
+	public void removeAlumno(Alumno alumno) {
 		this.alumnos.remove(alumno);
 	}
-
-
 
 	public List<Examen> getExamenes() {
 		return examenes;
 	}
 
-
-
 	public void setExamenes(List<Examen> examenes) {
 		this.examenes = examenes;
 	}
-	
+
 	public void addExamen(Examen examen) {
 		this.examenes.add(examen);
 	}
@@ -113,7 +116,20 @@ public class Curso {
 	public void removeExamen(Examen examen) {
 		this.examenes.remove(examen);
 	}
-	
-	
 
+	public List<CursoAlumno> getCursoAlumnos() {
+		return cursoAlumnos;
+	}
+
+	public void setCursoAlumnos(List<CursoAlumno> cursoAlumnos) {
+		this.cursoAlumnos = cursoAlumnos;
+	}
+	
+	public void addCursoAlumno(CursoAlumno cursoAlumno) {
+		this.cursoAlumnos.add(cursoAlumno);
+	}
+	
+	public void removeCursoAlumno(CursoAlumno cursoAlumno) {
+		this.cursoAlumnos.remove(cursoAlumno);
+	}
 }
