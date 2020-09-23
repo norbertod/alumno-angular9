85b3f77f926d5d75153ccef24be328edac114616
norbertod <norbertod.rojas@gmail.com>
2020-09-22 23:08:03 -0300

    clase 111 creando el proyecto y rutas

---
+++ cursos/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AlumnosComponent } from './components/alumnos/alumnos.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { ExamenesComponent } from './components/examenes/examenes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AlumnosComponent,
+    CursosComponent,
+    ExamenesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
